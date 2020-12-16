aa654b52410191bda3bf21d92633502b0ae18191
hamzahdili2001 <hdilihamza955@gmail.com>
2020-12-16 20:15:11 +0100

    First commit

---
+++ code.js
@@ -0,0 +1,8 @@
+/*
+# Documentation:
+	1: Variables: (var : "You Can Get it for any place ") - (let : Used in one Place ) - (const : Never Changed ) ;
+
+	2: Operatiion: (aa -= 7; : a = a - 7) - (a += 7; : a = a + 7) And (++ a = a + 1) (*= a = a + 1) and /= 
+
+*/
+
